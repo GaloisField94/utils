1d5353b89f2e0b8b6bd021f5a7cdbf3ea95ddcb4
Daniel Trędewicz <daniel.tredewicz@outlook.com>
2022-04-05 17:55:43 +0200

    natural_add bugfix; dword_t

--- src/natural_t.c
+++ src/natural_t.c
@@ -3,25 +3,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define word_width 64
+#define word_half_width (word_width / 2)
+
 typedef uint64_t word_t; 
+typedef word_t dword_t[2];
 typedef struct _natural_t {
     word_t* words;
     size_t capacity;
     size_t size;
 } natural_t;
 
-static void swap(void** op1, void** op2) {
-    void* tmp = *op1;
-    *op1 = *op2;
-    *op2 = tmp; 
-}
-
-natural_t* from_str(const char* str, uint8_t base) {
+natural_t* natural_from_str(const char* str, uint8_t base) {
     (void)str; (void)base;
     return malloc(sizeof(natural_t));
 }
 
-natural_t* from_unsigned(uint64_t n) {
+natural_t* natural_from_unsigned(uint64_t n) {
     natural_t* result = malloc(sizeof(natural_t));
     result->words = malloc(sizeof(word_t));
     result->words[0] = n;
@@ -30,7 +28,7 @@ natural_t* from_unsigned(uint64_t n) {
     return result;
 }
 
-natural_t* clone(const natural_t* src) {
+natural_t* natural_clone(const natural_t* src) {
     natural_t* result = malloc(sizeof(natural_t));
     result->words = malloc(sizeof(word_t) * src->size);
     for(size_t i = 0; i < src->size; i++) result->words[i] = src->words[i];
@@ -39,28 +37,28 @@ natural_t* clone(const natural_t* src) {
     return result;
 }
 
-natural_t* add(const natural_t* op1, const natural_t* op2) {
+natural_t* natural_add(const natural_t* op1, const natural_t* op2) {
     word_t carry = 0;
-    //assure op1->size >= op2->size
-    if(op1->size < op2->size) swap(&op1, &op2);
+    const natural_t* widther = (op1->size >= op2->size ? op1 : op2);
+    const natural_t* thinner = (op1->size < op2->size ? op1 : op2);
     natural_t* result = malloc(sizeof(natural_t));
-    result->size = op1->size;
+    result->size = widther->size;
     result->capacity = result->size + 1;
     result->words = malloc(sizeof(word_t) * result->capacity);
-    for (size_t i = 0; i < op2->size; i++) {
-        result->words[i] = op1->words[i] + op2->words[i] + carry;
-        carry = result->words[i] < op1->words[i];
+    for (size_t i = 0; i < thinner->size; i++) {
+        result->words[i] = widther->words[i] + thinner->words[i] + carry;
+        carry = result->words[i] < widther->words[i];
     }
-    for (size_t i = op2->size; i < op1->size; i++) {
-        result->words[i] = op1->words[i] + carry;
-        carry = result->words[i] < op1->words[i];
+    for (size_t i = thinner->size; i < widther->size; i++) {
+        result->words[i] = widther->words[i] + carry;
+        carry = result->words[i] < widther->words[i];
     }
     result->words[result->size] = carry;
     result->size += carry;
     return result;    
 }
 
-natural_t* add_unsigned(const natural_t* op1, uint64_t op2) {
+natural_t* natural_add_unsigned(const natural_t* op1, uint64_t op2) {
     natural_t* result = malloc(sizeof(natural_t));
     result->size = op1->size;
     result->capacity = result->size + 1;
@@ -76,8 +74,27 @@ natural_t* add_unsigned(const natural_t* op1, uint64_t op2) {
     return result;    
 }
 
-void add_to(natural_t* rop, const natural_t* op) {
+void natural_add_to(natural_t* rop, const natural_t* op) {
     (void)rop; (void)op;
+}
+
+static word_t add_dwords_carry(dword_t c, const dword_t a, const dword_t b) {
+	c[1] = a[1] + b[1];
+	c[0] = a[0] + b[0] + (word_t)(c[1] < a[1]); //identify overflow - true <=> sum < elem
+	return (word_t)(c[0] < a[0]); //identify overflow - true <=> sum < elem
+}
+
+static void mult_words(dword_t c, const word_t a, const word_t b) {
+	word_t a0 = a >> word_half_width, a1 = a & 0xFFFFFFFF;
+	word_t b0 = b >> word_half_width, b1 = b & 0xFFFFFFFF;
+	word_t mid_term = a0 * b1 + a1 + b0;
+	c[0] = a0 * b0 + (mid_term >> word_half_width); //c = 2^{64}a0b0 + 2^{32}(a0b1 + a1b0) + a1b1
+	c[1] = a1 * b1 + (mid_term << word_half_width); //thus upper half of mid term goes to lower half of c[0] and lower to upper of c[1]
+}
+
+natural_t* natural_mul(const natural_t* op1, const natural_t* op2) {
+	(void)op1; (void)op2;
+	return natural_from_unsigned(0);
 }
 
 size_t natural_size(const natural_t* op) {
@@ -93,4 +110,10 @@ void natural_print_words(const natural_t* op) {
         printf("0x%lX, ", op->words[i]);
     }
     printf("\n");
+}
+
+void natural_swap(natural_t** op1, natural_t** op2) {
+    natural_t* tmp = *op1;
+    *op1 = *op2;
+    *op2 = tmp; 
 }
